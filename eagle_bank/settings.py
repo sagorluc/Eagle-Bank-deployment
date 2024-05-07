cff29f06ac725697dfef447f79b05507caea93af
Md Sagar Ali <66152136+sagorluc@users.noreply.github.com>
2024-05-07 15:35:03 +0600

    Update settings.py
    update of allowing host and debug

--- eagle_bank/settings.py
+++ eagle_bank/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*p4#3n$&4($^#xomcfs&m7m*t_9hid%ajvl1qals^d6(t=%#2@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
